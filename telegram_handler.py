e7be2f0da5294972d07e679bef3579a68bc7c0c6
DIABLik666 <di-devil@yandex.ru>
2021-12-19 01:19:05 +0700

    Sprint: 7, Commit: 3

--- telegram_handler.py
+++ telegram_handler.py
@@ -8,8 +8,11 @@ class TelegramHandler(Handler):
         Handler.__init__(self)
         self.token = token
         self.chat_id = chat_id
+
+    def init_bot(self):
         self.bot = telegram.Bot(token=self.token)
 
     def emit(self, record):
+        self.init_bot()
         message = self.format(record)
         self.bot.send_message(chat_id=self.chat_id, text=message)
